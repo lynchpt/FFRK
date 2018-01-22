50f0e123d83da4c9273b2149879c4a722f3f1ee7
Patrick Lynch <patrick.lynch@outlook.com>
2018-01-21 23:05:27 -0500

    fixed issue where french formatted numbers came through Google as English formatted, and we had to recognize that

--- Model.EnlirTransform/Converters/DoubleConverter.cs
+++ Model.EnlirTransform/Converters/DoubleConverter.cs
@@ -11,12 +11,55 @@ namespace FFRKApi.Model.EnlirTransform.Converters
         {
             double result = 0;
 
+            //the source spreadsheet has number formatted in french way, as in 1,80.
+            //However, at least sometimes the Google Sheets api spontaneously converts those number into English format, like 1.80
+            //this way we can detect what is coming in and pick the right formula.
+            //Obviously, this only works because we know we will not be getting any number greater than the hundreds.
+            if (input.Contains(","))
+            {
+                result = ConvertFromStringToDoubleFrench(input);
+            }
+            else
+            {
+                result = ConvertFromStringToDoubleEnglish(input);
+            }
+
+            return result;
+        }
+
+        private double ConvertFromStringToDoubleFrench(string input)
+        {
+            double result = 0;
+
             if (!String.IsNullOrWhiteSpace(input))
             {
                 //input number from enlir are in French format when using group separators
                 NumberFormatInfo provider = new NumberFormatInfo();
                 provider.NumberDecimalSeparator = ",";
                 provider.NumberGroupSeparator = ".";
+                provider.NumberGroupSizes = new int[] { 3 };
+
+                bool converted = Double.TryParse(input, NumberStyles.AllowDecimalPoint, provider, out double candidateResult);
+
+                if (converted)
+                {
+                    result = candidateResult;
+                }
+            }
+
+            return result;
+        }
+
+        private double ConvertFromStringToDoubleEnglish(string input)
+        {
+            double result = 0;
+
+            if (!String.IsNullOrWhiteSpace(input))
+            {
+                //input number from enlir are in French format when using group separators
+                NumberFormatInfo provider = new NumberFormatInfo();
+                provider.NumberDecimalSeparator = ".";
+                provider.NumberGroupSeparator = ",";
                 provider.NumberGroupSizes = new int[] { 3 };
 
                 bool converted = Double.TryParse(input, NumberStyles.AllowDecimalPoint, provider, out double candidateResult);
