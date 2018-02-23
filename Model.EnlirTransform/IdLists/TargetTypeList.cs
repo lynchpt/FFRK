249c0bdb4cf626501776845d053dd24635995202
Patrick Lynch <patrick.lynch@outlook.com>
2018-02-23 13:24:01 -0500

    should have already happened

--- Model.EnlirTransform/IdLists/TargetTypeList.cs
+++ Model.EnlirTransform/IdLists/TargetTypeList.cs
@@ -9,20 +9,19 @@ namespace FFRKApi.Model.EnlirTransform.IdLists
         private readonly IList<KeyValuePair<int, string>> _typeList = new List<KeyValuePair<int, string>>()
                                                                       {
                                                                           new KeyValuePair<int, string>(0, "Unknown"),
-                                                                          new KeyValuePair<int, string>(1, String.Empty),
-                                                                          new KeyValuePair<int, string>(2, "-"),
-                                                                          new KeyValuePair<int, string>(3, "All allies"),
-                                                                          new KeyValuePair<int, string>(4, "All enemies"),
-                                                                          new KeyValuePair<int, string>(5, "Ally with status"),
-                                                                          new KeyValuePair<int, string>(6, "Another ally"),
-                                                                          new KeyValuePair<int, string>(7, "Lowest HP% ally"),
-                                                                          new KeyValuePair<int, string>(8, "Random ally"),
-                                                                          new KeyValuePair<int, string>(9, "Random enemies"),
-                                                                          new KeyValuePair<int, string>(10, "Random enemy"),
-                                                                          new KeyValuePair<int, string>(11, "Self"),
-                                                                          new KeyValuePair<int, string>(12, "Single"),
-                                                                          new KeyValuePair<int, string>(13, "Single ally"),
-                                                                          new KeyValuePair<int, string>(14, "Single enemy")
+                                                                          new KeyValuePair<int, string>(1, "-"),
+                                                                          new KeyValuePair<int, string>(2, "All allies"),
+                                                                          new KeyValuePair<int, string>(3, "All enemies"),
+                                                                          new KeyValuePair<int, string>(4, "Ally with status"),
+                                                                          new KeyValuePair<int, string>(5, "Another ally"),
+                                                                          new KeyValuePair<int, string>(6, "Lowest HP% ally"),
+                                                                          new KeyValuePair<int, string>(7, "Random ally"),
+                                                                          new KeyValuePair<int, string>(8, "Random enemies"),
+                                                                          new KeyValuePair<int, string>(9, "Random enemy"),
+                                                                          new KeyValuePair<int, string>(10, "Self"),
+                                                                          new KeyValuePair<int, string>(11, "Single"),
+                                                                          new KeyValuePair<int, string>(12, "Single ally"),
+                                                                          new KeyValuePair<int, string>(13, "Single enemy")
                                                                       };
 
 
