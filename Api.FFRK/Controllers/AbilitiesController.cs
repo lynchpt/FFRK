4e7e1abc63bfdea965f12c7904d8f37faf1d2bc0
Lynch, Patrick <patrick.lynch@outlook.com>
2018-03-06 17:41:16 -0500

    implemented documentation in Abilities controller

--- Api.FFRK/Controllers/AbilitiesController.cs
+++ Api.FFRK/Controllers/AbilitiesController.cs
@@ -51,14 +51,11 @@ namespace FFRKApi.Api.FFRK.Controllers
         /// <summary>
         /// Gets all Abilities and their properties, including required orbs
         /// </summary>
-        /// <remarks> Header of Remarks
-        /// 
-        /// Line 1
-        /// <b/>
-        /// Line 2
-        /// 
-        /// Line 3
-        /// 
+        /// <remarks>
+        /// Use Case - If you only need to access details for a small number of Abilities, it is faster to get each individual ability instance using a separate api call, but if 
+        /// you need to access most of them, it will be faster to call this api to get them all at once and store them locally so you can use them repeatedly.
+        /// <br /> 
+        /// Example - http://ffrkapi.azurewebsites.net/api/v1.0/Abilities (or use Try It Out to see data in this page)
         /// </remarks>
         /// <response code="200">
         ///     <see>IEnumerable&lt;D.Ability&gt;</see>
@@ -81,9 +78,18 @@ namespace FFRKApi.Api.FFRK.Controllers
         /// <summary>
         /// Gets one Ability by its unique id
         /// </summary>
-        /// <param name="abilityId">Param Info: about abilityId</param>
-        /// <returns>return info</returns>
-        /// <response code="200"><see cref="D.Ability"/></response>
+        /// <remarks>
+        /// Sample Use Case - You want to find out data about the Ability called "Firaja"
+        /// - You first call /api/v1.0/IdLists/Ability to get the proper IdList
+        /// - Then you look up the integer Key associated with the Value of "Firaja" in the IdList (the id is 4 in this case)
+        /// - Finally you call this api: api/v1.0/Abilities/4
+        /// <br /> 
+        /// Example - http://ffrkapi.azurewebsites.net/api/v1.0/Abilities/4 (or use Try It Out to see data in this page)
+        /// </remarks>
+        /// <param name="abilityId">the integer id for the desired Ability; it can be found in the Ability IdList</param>
+        /// <response code="200">
+        ///     <see>IEnumerable&lt;D.Ability&gt;</see>
+        /// </response>
         [HttpGet]
         [Route(RouteConstants.AbilitiesRoute_Id)]
         [SwaggerOperation(nameof(GetAbilitiesById))]
@@ -99,6 +105,21 @@ namespace FFRKApi.Api.FFRK.Controllers
             return new ObjectResult(result);
         }
 
+        /// <summary>
+        /// Gets all Abilities that have the specified AbilityType
+        /// </summary>
+        /// <remarks>
+        /// Sample Use Case - You want to find out data about all Abilities that have an AbilityType of "NIN"
+        /// - You first call /api/v1.0/TypeLists/AbilityType to get the proper TypeList
+        /// - Then you look up the integer Key associated with the Value of "NIN" in the TypeList (the id is 5 in this case)
+        /// - Finally you call this api: api/v1.0/Abilities/AbilityType/5";
+        /// <br /> 
+        /// Example - http://ffrkapi.azurewebsites.net/api/v1.0/Abilities/AbilityType/5 (or use Try It Out to see data in this page)
+        /// </remarks>
+        /// <param name="abilityType">the integer id for the desired AbilityType; it can be found in the AbilityType TypeList</param>
+        /// <response code="200">
+        ///     <see>IEnumerable&lt;D.Ability&gt;</see>
+        /// </response>
         [HttpGet]
         [Route(RouteConstants.AbilitiesRoute_AbilityType)]
         [SwaggerOperation(nameof(GetAbilitiesByAbilityType))]
@@ -114,6 +135,19 @@ namespace FFRKApi.Api.FFRK.Controllers
             return new ObjectResult(result);
         }
 
+        /// <summary>
+        /// Gets all Abilities that have the specified Rarity
+        /// </summary>
+        /// <remarks>
+        /// Sample Use Case - You want to find out data about all Abilities that have an Rarity of 5 (as in 5*)
+        /// - You can straight away call this api: api/v1.0/Abilities/Rarity/5";
+        /// <br /> 
+        /// Example - http://ffrkapi.azurewebsites.net/api/v1.0/Abilities/Rarity/5 (or use Try It Out to see data in this page)
+        /// </remarks>
+        /// <param name="rarity">the integer rarity that all returned Abilities need to share</param>
+        /// <response code="200">
+        ///     <see>IEnumerable&lt;D.Ability&gt;</see>
+        /// </response>
         [HttpGet]
         [Route(RouteConstants.AbilitiesRoute_Rarity)]
         [SwaggerOperation(nameof(GetAbilitiesByRarity))]
@@ -129,6 +163,21 @@ namespace FFRKApi.Api.FFRK.Controllers
             return new ObjectResult(result);
         }
 
+        /// <summary>
+        /// Gets all Abilities that have the specified SchoolType
+        /// </summary>
+        /// <remarks>
+        /// Sample Use Case - You want to find out data about all Abilities that have a SchoolType of "Dancer"
+        /// - You first call /api/v1.0/TypeLists/SchoolType to get the proper TypeList
+        /// - Then you look up the integer Key associated with the Value of "Dancer" in the TypeList (the id is 6 in this case)
+        /// - Finally you call this api: api/v1.0/Abilities/School/6";
+        /// <br /> 
+        /// Example - http://ffrkapi.azurewebsites.net/api/v1.0/Abilities/School/6 (or use Try It Out to see data in this page)
+        /// </remarks>
+        /// <param name="abilityType">the integer id for the desired SchoolType; it can be found in the SchoolType TypeList</param>
+        /// <response code="200">
+        ///     <see>IEnumerable&lt;D.Ability&gt;</see>
+        /// </response>
         [HttpGet]
         [Route(RouteConstants.AbilitiesRoute_School)]
         [SwaggerOperation(nameof(GetAbilitiesBySchool))]
@@ -144,6 +193,21 @@ namespace FFRKApi.Api.FFRK.Controllers
             return new ObjectResult(result);
         }
 
+        /// <summary>
+        /// Gets all Abilities that have the specified ElementType
+        /// </summary>
+        /// <remarks>
+        /// Sample Use Case - You want to find out data about all Abilities that have an ElementType of "Fire"
+        /// - You first call /api/v1.0/TypeLists/ElementType to get the proper TypeList
+        /// - Then you look up the integer Key associated with the Value of "Fire" in the TypeList (the id is 4 in this case)
+        /// - Finally you call this api: api/v1.0/Abilities/Element/4;
+        /// <br /> 
+        /// Example - http://ffrkapi.azurewebsites.net/api/v1.0/Abilities/Element/4 (or use Try It Out to see data in this page)
+        /// </remarks>
+        /// <param name="abilityType">the integer id for the desired ElementType; it can be found in the ElementType TypeList</param>
+        /// <response code="200">
+        ///     <see>IEnumerable&lt;D.Ability&gt;</see>
+        /// </response>
         [HttpGet]
         [Route(RouteConstants.AbilitiesRoute_Element)]
         [SwaggerOperation(nameof(GetAbilitiesByElement))]
@@ -159,6 +223,39 @@ namespace FFRKApi.Api.FFRK.Controllers
             return new ObjectResult(result);
         }
 
+        /// <summary>
+        /// Gets all Abilities that match all of the considered criteria in the submitted search template/specification
+        /// </summary>
+        /// <remarks>
+        /// While you pass in a full Ability object as a search template, only the following fields are used in the search (all others are ignored):
+        /// - AbilityName (comparison is Contains, not exact match)
+        /// - AbilityType
+        /// - AutoTargetType 
+        /// - CastTime (value specified or less)
+        /// - DamageFormulaType
+        /// - Elements (only the first in the list is considered)
+        /// - Multiplier (value specified or greater)
+        /// - OrbRequirements (only the first in the list is considered)
+        /// - Rarity
+        /// - School 
+        /// - SoulBreakPointsGained (value specified or greater)
+        /// - TargetType
+        /// 
+        /// Any of the above considered fields that you leave blank in the template object are NOT considered as part of the search. 
+        /// Any of the above considered fields that you specify in the template object must ALL be matched by any Ability in order for it to be returned in the search.
+        /// 
+        /// Sample Use Case - You want to find out data about all Abilities that have an ElementType of "Fire" and a SchoolType of Black Magic
+        /// - You first call Type List Apis to get the ids for Element = Fire and SchoolType = Black Magic (4 and 3)
+        /// - You create an Ability object and fill in the above ids into the Element collection and School property
+        /// - You attach the Ability specification object as the body of a Post request.
+        /// - Finally you call this api: api/v1.0/Abilities/Search [POST];
+        /// <br /> 
+        /// Example - http://ffrkapi.azurewebsites.net/api/v1.0/Abilities/Search (or use Try It Out to see data in this page)
+        /// </remarks>
+        /// <param name="searchPrototype">the Ability object that contains the search criteria</param>
+        /// <response code="200">
+        ///     <see>IEnumerable&lt;D.Ability&gt;</see>
+        /// </response>
         [HttpPost]
         [Route(RouteConstants.AbilitiesRoute_Search)]
         [SwaggerOperation(nameof(GetAbilitiesBySearch))]
